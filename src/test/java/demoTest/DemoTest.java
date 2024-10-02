9d4e0ef5266155d7f7bf316e570d27c3374e4157
pradip <pradippatil1543@gmail.com>
2024-10-02 17:00:54 +0530

    added 2nd test case

--- src/test/java/demoTest/DemoTest.java
+++ src/test/java/demoTest/DemoTest.java
@@ -2,18 +2,24 @@ package demoTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DemoTest {
 
-	public static void main(String[] args) {
+	@Test
+	public void visitGoogle() {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
-		System.out.println("added console log");
 
+	}
+	
+	@Test
+	public void simpleConsoleLog() {
+		System.out.println("this is console log test case");
 	}
 
 }
