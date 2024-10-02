a26f7c879a5b4687c6cfb737d5d071bbbeafef6f
pradip <pradippatil1543@gmail.com>
2024-10-02 16:45:55 +0530

    added test cases

--- src/main/java/demo/Sample.java
+++ src/main/java/demo/Sample.java
@@ -2,17 +2,19 @@ package demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Sample {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	@Test
+	public void visitBrowserTest() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://www.google.com/");
+
 	}
 
 }
